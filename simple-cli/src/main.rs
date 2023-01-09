0b56ae6b1f52f082736f91d2cbcfcc53c5051d79
ToshikiHata <toshiki_hata@f4samurai.jp>
2023-01-09 21:00:10 +0900

    簡単なCLIを作る: 入力を受ける

--- simple-cli/src/main.rs
+++ simple-cli/src/main.rs
@@ -1,11 +1,13 @@
 use clap::Parser;
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
 
 #[derive(Parser, Debug)]
 #[clap(
     name = "My RPN program",
     version = "1.0.0",
     author = "Toshiki Hata",
-    about = "Simple RPN calculator",
+    about = "Simple RPN calculator"
 )]
 struct Opts {
     #[clap(short, long)]
@@ -18,10 +20,27 @@ struct Opts {
 fn main() {
     let opts = Opts::parse();
 
-    match opts.formula_file {
-            Some(file) => println!("File specified: {}", file),
-            None => println!("No file specified."),
-        }
-    println!("Is verbosity specified?: {}", opts.verbose);
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+        run(reader, opts.verbose);
+    } else {
+        // println!("No file is specified")
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose);
+    }
+}
 
+
+// ※トレイト境界は､以下のように書いても同じ
+// fn run<R: BufRead>(reader: R, verbose: bool) { /**/ }
+fn run<R>(reader: R, verbose: bool)
+where
+    R: BufRead,
+{
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+    }
 }
